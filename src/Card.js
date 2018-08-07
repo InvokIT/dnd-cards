0d417eae034de4e36e618df41fce5ead90dd81c8
Sebastian Joergensen <kons-sj@nationalbanken.dk>
2018-08-07 15:42:52 +0200

    more text inputs

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,6 @@ import React from 'react';
 import classNames from "classnames";
 import style from './Card.css';
 
-const BoundInput = ({name, value, onChange}) => (
-    <input type="text" value={value} onChange={onChange.bind(name)} />
-);
-
 const Card = ({
     name,
     school,
@@ -27,8 +23,21 @@ const Card = ({
                 <header className={style.name}>
                     <input type="text" value={name} onChange={onPropertyChange.bind("name")} />
                 </header>
+                <section className={style.school}>
+                    <input type="text" value={school} onChange={onPropertyChange.bind("school")} />
+                </section>
+                <section className={style.range}>
+                    <input type="text" value={range} onChange={onPropertyChange.bind("range")} />
+                </section>
+                <section className={style.duration}>
+                    <input type="text" value={duration} onChange={onPropertyChange.bind("duration")} />
+                </section>
                 <section className={style.description}>
                     <textarea onChange={onPropertyChange.bind("description")}>{description}</textarea>
+                </section>
+                <section className={style.bookRef}>
+                    <div className={style.bookRefName}>{bookRef.name}</div>
+                    <div className={style.bookRefPage}>{bookRef.page}</div>
                 </section>
             </div>
         </article>
