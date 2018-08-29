250737f09dd17602e5b36193fe60ab8e2f1063aa
Sebastian Jørgensen <sj@invokit.dk>
2018-08-29 13:46:13 +0200

    removed unused code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SpellsDataLists from "./SpellsDataLists";
 import ToolbarContainer from "./ToolbarContainer";
